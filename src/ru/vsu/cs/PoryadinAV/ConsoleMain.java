9593207805d6fe8a46c1344bfce739ae67255c6c
G4st3r21 <aporyadin003@gmail.com>
2021-12-24 14:24:44 +0300

    update

--- src/ru/vsu/cs/PoryadinAV/ConsoleMain.java
+++ src/ru/vsu/cs/PoryadinAV/ConsoleMain.java
@@ -4,29 +4,6 @@ import ru.vsu.cs.PoryadinAV.utils.ArrayUtils;
 
 import java.io.PrintStream;
 
-/**
- * Чтение и запись из текст. файла
- * Программа в двух видах: консоль, с разбором параметров ком. строки и оконное приложение в JTable
- * 5 тестов в текстовых файлах(input01.txt, input02.txt...)
- * Для разбора параметров командной строки реализовать функцию: public static InputArgs parseCmdArgs(String[] args)
- * int[][] array1 = {
- * {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
- * {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
- * {0, 1, 2, 3, 10, 5, 6, 7, 8, 9},
- * {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
- * {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
- * {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
- * };
- * int[][] array2 = {
- * {10, 5, 6, 7, 8, 9},
- * {4, 5, 6, 7, 8, 9},
- * {4, 5, 6, 7, 8, 9},
- * {4, 5, 6, 7, 8, 9}
- * };
- * <p>
- * .\src\ru\vsu\cs\PoryadinAV\input1.txt .\src\ru\vsu\cs\PoryadinAV\output1.txt
- */
-
 public class ConsoleMain {
 
     public static class CmdParams {
